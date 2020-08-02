c1053d3081cb526375ec5dedda5bd12005fa5ed2
Elysa <68788692+elysaph@users.noreply.github.com>
2020-08-02 12:36:55 -0300

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -11,5 +11,13 @@ def primary():
 
   print(quotes[rnd])
 
+
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  last = len (quotes) - 1
+  rnd = random.randint(0, last)
+
 if __name__== "__main__":
   primary()
