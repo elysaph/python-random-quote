9ce7ad5ecc6b58fb144c47e90b5a9e9a629b30b9
Elysa <68788692+elysaph@users.noreply.github.com>
2020-08-19 12:48:56 -0300

    changed what was asked by the bot tutorial

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,23 @@
-def main():
-  print("Keep it logically awesome.")
+import random
+def primary():
+  #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  last = len (quotes) - 1
+  rnd = random.randint(0, last)
+
+  print(quotes[rnd])
+
+
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  last = len (quotes) - 1
+  rnd = random.randint(0, last)
 
 if __name__== "__main__":
-  main()
-  python3 get-quote.py
+  primary()
